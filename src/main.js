f8dd1f457e462500892521bf663701427cb071ca
yang_going <2210744381@qq.com>
2020-05-11 22:36:22 +0800

    项目第一次完成

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import './assets/css/global.css'
+import axios from 'axios'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+axios.defaults.baseURL = 'http://localhost:9999'
+Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
 
 new Vue({
