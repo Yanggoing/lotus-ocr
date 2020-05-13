8eb3767274200fbd2ca047fbfe587cc62519882e
yang_going <2210744381@qq.com>
2020-05-13 23:49:34 +0800

    修复了一些bug

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-const Home = () => import( /* webpackChunkName: "home" */ '../components/Home.vue')
+// const Home = () => import( /* webpackChunkName: "home" */ '../components/Home.vue')
 
-
+import Home from '../components/Home.vue'
 Vue.use(VueRouter)
 
 const routes = [{
